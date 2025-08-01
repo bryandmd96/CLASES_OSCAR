044fc8eef6479d15af82fae41bfdb9dc9f0b7e96
Bryandmd96 <bryandmd96@gmail.com>
2025-08-01 14:04:34 +0200

    clase 4 pacial

---
+++ clase_4/js/main__09.js
@@ -0,0 +1,26 @@
+/* const titulo = document.getElementById('titulo');
+const parrafo = document.getElementsByClassName('texto');
+const botones = document.getElementsByTagName('button');
+
+const parrafo2 = document.querySelector('.texto');
+const botones2 = document.querySelectorAll('button')
+
+titulo.textContent = "HOLA MUNDO";
+titulo.style.color = 'blue';
+titulo.setAttribute('class', 'nuevo-estilo');
+
+const nuevoParrafo = document.createElement('p');
+nuevoParrafo.textContent = 'Soy nuevo!';
+document.body.appendChild(nuevoParrafo);
+
+const boton = document.querySelector('button');
+
+// ojo desde aqui que me perdi
+
+boton.addEventListener('click'), () => {
+    alert('¡click detectado!');
+    boton.getElementById('titulo').textContent = 'Texto cambiado';
+}
+
+ */
+
